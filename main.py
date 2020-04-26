b8f329b056de46bd2c6374e088dfc3277ea43905
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2020-04-26 12:21:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ if __name__ == '__main__':
 
 	APP_KEY=os.environ['APP_KEY']
 
+
 	default_args = {
 		"APP_KEY": APP_KEY,
-		"PAGE_SIZE": None,
-		"NUM_PAGE": None,
-		"OUTPUT": None,
+		"page_size": None,
+		"num_pages": None,
+		"output": None,
 	}
 
 
@@ -27,22 +28,19 @@ if __name__ == '__main__':
 			
 
 	if default_args["APP_KEY"]==None:
-		raise Exception('Ya didn\'t give me Missing APP KEY!!!')
+		raise Exception('missing APP KEY!!!')
 
-	if default_args["PAGE_SIZE"]==None:
-		raise Exception('Ya got to specify page size!!!')
+	if default_args["page_size"]==None:
+		raise Exception('ya got to specify page size!!!')
 
-	if default_args["NUM_PAGE"]==None:
-		print('Ya didn\'t tell me about the Number of Page you want, I am gonna get all of...' )
-
-
+	if default_args["num_pages"]==None:
+		print('ya didn\'t told me about the Number of Page you want, I am gonna get all of...' )
 
 
-	get_data1(default_args["APP_KEY"],default_args["PAGE_SIZE"],default_args["NUM_PAGE"])
+	raw_data=get_data1(default_args["APP_KEY"],default_args["page_size"],default_args["num_pages"])
 
-
-	with open(default_args["OUTPUT"],"w") as fw:
+	with open(default_args["output"],"w") as fw:
 	
-		for item in data:
+		for item in raw_data:
 			for j in item: 
 				fw.write(str(j)+'\n')
